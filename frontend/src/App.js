c4879c9f4c9c67af1a2589297468fead306d921b
Priyanshu <priyanshu022017@gmail.com>
2024-07-03 08:36:05 +0530

    files added

---
+++ frontend/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import BingeWatch from "./pages/BingeWatch";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<BingeWatch />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
